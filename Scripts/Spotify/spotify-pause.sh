d320749fb2b5157a931185e20043e6e368c079bc
husseinhareb <shtam.hareb12@gmail.com>
2023-09-17 21:24:26 +0300

    pause or play the song

--- Scripts/Spotify/spotify-pause.sh
+++ Scripts/Spotify/spotify-pause.sh
@@ -8,3 +8,7 @@ status="${status%\"*}"
 if [ "$status" == "Playing" ]; then
     dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null 2>&1
     echo "󰐊"
+else
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play > /dev/null 2>&1
+    echo "󰏤"
+fi
