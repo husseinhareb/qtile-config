f4822e8438ca3deccff691336fd63785280d9059
husseinhareb <shtam.hareb12@gmail.com>
2023-09-18 11:36:56 +0300

    power button script

---
+++ Scripts/Power/power.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+uptime=$(uptime -p | sed -e 's/up //g')
+dir="~/.config/rofi/"
+rofi_command="rofi -theme $dir/power.rasi"
+
+# Options
+terminal=""
+shutdown="󰐥"
+
+logout=""
