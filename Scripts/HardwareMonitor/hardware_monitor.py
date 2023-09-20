df0cb48f154803a9ad681bd64fc3485413d0e9fe
husseinhareb <shtam.hareb12@gmail.com>
2023-09-20 16:37:56 +0300

    widget for minotoring hardware

--- Scripts/HardwareMonitor/hardware_monitor.py
+++ Scripts/HardwareMonitor/hardware_monitor.py
@@ -1,5 +1,12 @@
 import subprocess
 import psutil
+import tkinter as tk
+import ttkbootstrap as tb
+import os
+from PIL import Image
+from ttkbootstrap.constants import *
+Image.CUBIC = Image.BICUBIC
+
 def get_cpu_temp():
     output = subprocess.check_output(["sensors"], text=True)
     for line in output.splitlines():
