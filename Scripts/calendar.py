4310bc7d1bb33158eb0b42c37b687288c0c7dd6f
husseinhareb <shtam.hareb12@gmail.com>
2023-09-21 20:50:30 +0300

    calendar.py

--- Scripts/calendar.py
+++ Scripts/calendar.py
@@ -7,3 +7,11 @@ from PIL import Image
 from ttkbootstrap.constants import *
 Image.CUBIC = Image.BICUBIC
 
+root = tk.Tk(className="monitoring_widget")
+theme = tb.Style("darkly")
+root.configure(bg="#3b3b3b")
+root.geometry('550x400+1725+38')
+
+tb.DateEntry(root, style='success.TCalendar')
+
+root.mainloop()
