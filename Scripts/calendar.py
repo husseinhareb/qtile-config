326afaba1549081f1cb01cb8436bbda0b8a0cd62
husseinhareb <shtam.hareb12@gmail.com>
2023-09-22 14:32:44 +0300

    calendar widget

--- Scripts/calendar.py
+++ Scripts/calendar.py
@@ -1,18 +1,14 @@
 import calendar
 import datetime
 
-# Get the current date
 current_date = datetime.date.today()
 
-# Extract the current month and year
 year = current_date.year
 month = current_date.month
 day = current_date.day
 
-# Create a TextCalendar object
-cal = calendar.TextCalendar(calendar.SUNDAY)  # Start weeks on Sunday
+cal = calendar.TextCalendar(calendar.SUNDAY)  
 
-# Display the calendar for the current month and year with today's date highlighted
 calendar_text = cal.formatmonth(year, month)
 highlighted_calendar = calendar_text.replace(f"{day:2}", f"\033[1;37;41m{day:2}\033[m", 1)
 
