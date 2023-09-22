42fe289984d47fa69251583051b08996747cd4f5
husseinhareb <shtam.hareb12@gmail.com>
2023-09-22 14:42:27 +0300

    calendar widget

--- Scripts/calendar.py
+++ Scripts/calendar.py
@@ -1,5 +1,8 @@
 import calendar
 import datetime
+import tkinter as tk
+import ttkbootstrap as tb
+
 
 current_date = datetime.date.today()
 
@@ -13,3 +16,12 @@ calendar_text = cal.formatmonth(year, month)
 highlighted_calendar = calendar_text.replace(f"{day:2}", f"\033[1;37;41m{day:2}\033[m", 1)
 
 print(highlighted_calendar)
+
+root = tk.Tk(className="monitoring_widget")
+theme = tb.Style("darkly")
+root.configure(bg="#3b3b3b")
+root.geometry('250x250+1725+38')
+
+
+
+root.mainloop()
