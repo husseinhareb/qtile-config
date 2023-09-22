1c3d391ad6f5a985c49655585d7f5efa5cbbbc05
husseinhareb <shtam.hareb12@gmail.com>
2023-09-22 21:37:38 +0300

    calendar widget

--- Scripts/calendar.py
+++ Scripts/calendar.py
@@ -1,27 +1,45 @@
+import tkinter as tk
 import calendar
 import datetime
-import tkinter as tk
-import ttkbootstrap as tb
-
 
 current_date = datetime.date.today()
 
 year = current_date.year
 month = current_date.month
-day = current_date.day
+day = current_date.day  # Extract the day of the month and convert to a string
+cal = calendar.TextCalendar(calendar.SUNDAY) 
+calendar_text =  cal.formatmonth(year, month)
+    
+root = tk.Tk()
+root.title("Linux-like Calendar")
 
-cal = calendar.TextCalendar(calendar.SUNDAY)  
+text_widget = tk.Text(root, wrap=tk.WORD)
+text_widget.pack()
 
-calendar_text = cal.formatmonth(year, month)
-highlighted_calendar = calendar_text.replace(f"{day:2}", f"\033[1;37;41m{day:2}\033[m", 1)
+text_widget.configure(font=("Courier", 12))
 
-print(highlighted_calendar)
+text_widget.insert("1.0", calendar_text)
 
-root = tk.Tk(className="monitoring_widget")
-theme = tb.Style("darkly")
-root.configure(bg="#3b3b3b")
-root.geometry('250x250+1725+38')
+text_start = "2.0"  # Start of the second line (where the dates start)
+text_end = text_widget.index(tk.END)
 
+while True:
+        # Find the position of today's date in the text
+    start_idx = text_widget.search(str(day), text_start, text_end)
 
+    if not start_idx:
+        break
 
+    end_idx = text_widget.index(f"{start_idx}+2c")  # Calculate the end position
+
+        # Tag the date with a red background
+    text_widget.tag_add("highlight", start_idx, end_idx)
+    text_widget.tag_configure("highlight", background="red", foreground="white")
+
+        # Update the start position for the next search
+    text_start = end_idx
+
+    # Start the Tkinter event loop
 root.mainloop()
+
+# Call the function to display the calendar
