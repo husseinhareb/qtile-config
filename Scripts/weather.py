47365daa47debc48241e8232c4055563d6e96fff
husseinhareb <shtam.hareb12@gmail.com>
2023-09-24 22:45:50 +0300

    weather script

---
+++ Scripts/weather.py
@@ -0,0 +1,27 @@
+import requests
+
+city_name = "Beirut"
+api_key = "2a33d8b44aa8d93d07feac453b4a79aa"
+unit = "metric" 
+temperature_unit = "C"
+
+icons_list = {
+    "01d": "", # Clear sky day.
+    "01n": "", # Clear sky night.
+    "02d": "", # Few clouds day.
+    "02n": "", # Few clouds night.
+    "03d": "", # Scattered clouds day.
+    "03n": "", # Scattered clouds night.
+    "04d": "", # Broken clouds day.
+    "04n": "", # Broken clouds night.
+    "09d": "", # Shower rain day.
+    "09n": "", # Shower rain night.
+    "10d": "", # Rain day.
+    "10n": "", # Rain night
+    "11d": "", # Thunderstorm day.
+    "11n": "", # Thunderstorm night
+    "13d": "", # Snow day. Snowflake alternative: 
+    "13n": "", # Snow night. Snowflake alternative: 
+    "50d": "", # Mist day.
+    "50n": "" # Mist night.
+}
