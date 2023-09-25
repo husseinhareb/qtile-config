35eed2064b0841530180caf78e8141c36e917bf7
husseinhareb <shtam.hareb12@gmail.com>
2023-09-25 23:36:54 +0300

    wifi widget

---
+++ Scripts/Wifi/wifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wifi_list=$(nmcli -t -f SSID,SIGNAL dev wifi list)
+
+formatted_list=""
+while IFS=':' read -r ssid signal; do
+    if ((signal < 30)); then
+        signal_symbol="󰤟"
+    elif ((signal < 60)); then
+        signal_symbol="󰤢"
+    elif ((signal < 90)); then
+        signal_symbol="󰤥"
+    else
+        signal_symbol="󰤨" 
+    fi
+    formatted_list+=" $signal_symbol:$ssid\n"
+done <<< "$wifi_list"
+
+chosen=$(echo -e "$formatted_list" | rofi -theme ~/.config/rofi/wifi.rasi -dmenu -p "Wi-Fi networks:" | awk -F ':' '{print $2}' | xargs)
+
+if [ -n "$chosen" ]; then
+    nmcli device wifi connect "$chosen"
+fi
