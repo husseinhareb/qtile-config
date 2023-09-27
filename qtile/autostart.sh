18c915198db393e48f9b5d28aabf84c03ca4a185
husseinhareb <shtam.hareb12@gmail.com>
2023-09-27 23:04:51 +0300

    autostart script

---
+++ qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+(polybar power)&
+(polybar clock)&
+(polybar spotify)&
+(polybar workspaces)&
+(polybar monitor)&
+(polybar wifi)&
+(polybar settings)&
+(polybar sound)&
+(polybar weather)&
+
